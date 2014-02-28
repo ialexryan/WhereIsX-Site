ec550436b0c3c94340550c51f5f8a069ec79bff6
Alex Ryan <alex@alexryan.org>
2014-02-27 16:48:03 -0800

    _

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from authentication import *
 
 app = Flask(__name__)
 
+# Error Types
+ERR_MISSING_USER = 1
+
 default_user = "Jaden"
 
 global_user_location_dict = {default_user:"uninitialized"}
 
-@app.route('/error/<error_type>')
+@app.route('/error/<int:error_type>')
 def error(error_type):
-    if error_type == "missing user":
+    if error_type == ERR_MISSING_USER:
         return url_for(print_default_user)
     else:
         return "Unspecified error." + url_for(print_default_user)
@@ -23,14 +26,14 @@ def update_location(username, location):
         global_user_location_dict[username] = location
         return redirect(url_for('print_location', username=username))
     else:
-        return redirect(url_for('error', error_type="missing user"))
+        return redirect(url_for('error', error_type=ERR_MISSING_USER))
 
 @app.route('/<username>')
 def print_location(username):
     if username in global_user_location_dict:
         return username + " is " + global_user_location_dict[username] + "."
     else:
-        return redirect(url_for('error', error_type="missing user"))
+        return redirect(url_for('error', error_type=ERR_MISSING_USER))
 
 @app.route('/')
 def print_default_user():
